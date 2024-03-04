127ddf9483d6f2dabead91bc0ad8296a32d26d68
Rohit Joseph <rohitjosephrhea285@gmail.com>
2024-03-04 12:09:16 +0530

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# flask-render_template-
--- s.sh
+++
@@ -1,7 +0,0 @@
-echo "# flask-render_template-" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rj285/flask-render_template-.git
-git push -u origin main
